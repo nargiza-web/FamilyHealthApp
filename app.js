4774ebee7b4cc3a63e865871bd3c73e2e3751320
Ian Sear <iansear@Ians-MacBook-Pro.local>
2020-01-09 12:42:16 -0600

    test

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express()
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt')
-// const PORT = process.env.PORT || 8080
-const PORT = 3000
+const PORT = process.env.PORT || 8080
+// const PORT = 3000
 require('dotenv').config()
 const path = require('path')
 const VIEWS_PATH = path.join(__dirname,'/views')
