0f6363751400ac69a1d039efd9ed23760f561352
Ian Sear <iansear@Ians-MacBook-Pro.local>
2020-01-09 12:23:10 -0600

    testing my table, edited db host

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 const path = require('path')
 const VIEWS_PATH = path.join(__dirname,'/views')
 // const PORT = 3000
-
+const models = require("./models")
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.engine('mustache', mustacheExpress(VIEWS_PATH + './partials', '.mustache'))
@@ -19,7 +19,9 @@ app.get("/hello", (req,res)=>{
     res.send('Hello!')
 })
 
-
+app.get("/ian", (req, res) => {
+    models.CareProviders.findAll().then(providers => res.json(providers))
+})
 
 app.listen(PORT, ()=>{
     console.log("Server is running...")
