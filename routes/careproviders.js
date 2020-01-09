15fc95e5a61cca1158f14333c8ba7d682309ed32
Baylie <bayliewarrick2019@gmail.com>
2020-01-09 14:32:49 -0600

    fixed routers, added members router

--- routes/careproviders.js
+++ routes/careproviders.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-app.get("/careproviders", (req, res) => {
+router.get("/", (req, res) => {
     models.CareProviders.findAll().then(providers => res.render("test", {providers: providers}))
 })
 
