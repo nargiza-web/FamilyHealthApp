1287504505a22581ec13467e52caf46341ba34b5
Alejandra Guzman <aleguzman.gza@gmail.com>
2020-01-09 14:26:24 -0600

    lab test update

--- models/lab-result.js
+++ models/lab-result.js
@@ -1,12 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Lab - Result = sequelize.define('Lab-Result', {
+  const LabResult = sequelize.define('Lab-Result', {
     image: DataTypes.STRING,
     test_day: DataTypes.STRING,
     category: DataTypes.STRING
   }, {});
-  Lab - Result.associate = function(models) {
+  LabResult.associate = function(models) {
     // associations can be defined here
   };
-  return Lab - Result;
+  return LabResult;
 };
