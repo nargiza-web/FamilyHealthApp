347458d6820c3e266fc85e26d729925c413225eb
Alejandra Guzman <aleguzman.gza@gmail.com>
2020-01-09 15:56:30 -0600

    update

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ const careprovidersRouter = require('./routes/careproviders')
 const familiesRouter = require('./routes/families')
 const membersRouter = require('./routes/members')
 const medicationRouter = require('./routes/medications')
-const indexRouter = require('./routes/index')
+// const indexRouter = require('./routes/index')
 
 
-app.use('/', indexRouter)
+// app.use('/', indexRouter)
 app.use('/careproviders', careprovidersRouter)
 app.use('/families', familiesRouter)
 app.use('/members', membersRouter)
