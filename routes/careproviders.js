30338f497bc1f9d85066df5fb1c83f9eb11bbbfe
Alejandra Guzman <aleguzman.gza@gmail.com>
2020-01-09 14:58:37 -0600

    careproviders.js update

--- routes/careproviders.js
+++ routes/careproviders.js
@@ -1,12 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
-<<<<<<< Updated upstream
 router.get("/", (req, res) => {
-=======
-router.get("/careproviders", (req, res) => {
->>>>>>> Stashed changes
-    models.CareProviders.findAll().then(providers => res.render("test", {providers: providers}))
+   models.CareProviders.findAll().then(providers => res.render("test", {providers: providers}))
 })
 
 module.exports = router
